202c802bcd97fdc31ae56f62a540af0d70f8629f
Sid Shankar <sidshank@github.com>
2023-01-04 23:14:14 -0500

    Generalized version of Day 9 - Part 1

--- day9.rs
+++ day9.rs
@@ -2,6 +2,9 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
+    // Define a constant for the number of knots in the rope
+    const NUMBER_OF_KNOTS: u8 = 2;
+
     let lines = BufReader::new(File::open("day9.txt").unwrap()).lines();
 
     let moves: Vec<(char, u32)> = lines
@@ -14,78 +17,97 @@ fn main() {
         .collect();
 
     let mut visited = std::collections::HashSet::new();
-
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
+    let mut knots: Vec<(i32, i32)> = Vec::new();
+    for _ in 0..NUMBER_OF_KNOTS {
+        knots.push((0, 0));
+    }
 
     let mut add_to_visited = |x: i32, y: i32| {
         visited.insert(format!("{}-{}", x, y));
     };
 
-    add_to_visited(tail.0, tail.1);
+    add_to_visited(0, 0);
 
     for (c, n) in moves {
-        match c {
-            'U' => {
-                for _ in 0..n {
-                    head.1 += 1;
+        for k in 0..(NUMBER_OF_KNOTS - 1) as usize {
+            let head_index = k;
+            let tail_index = k + 1;
+
+            let mut head = knots[head_index];
+            let mut tail = knots[tail_index];
+            match c {
+                'U' => {
+                    for _ in 0..n {
+                        head.1 += 1;
+                    }
                 }
-            }
-            'D' => {
-                for _ in 0..n {
-                    head.1 -= 1;
+                'D' => {
+                    for _ in 0..n {
+                        head.1 -= 1;
+                    }
                 }
-            }
-            'R' => {
-                for _ in 0..n {
-                    head.0 += 1;
+                'R' => {
+                    for _ in 0..n {
+                        head.0 += 1;
+                    }
                 }
-            }
-            'L' => {
-                for _ in 0..n {
-                    head.0 -= 1;
+                'L' => {
+                    for _ in 0..n {
+                        head.0 -= 1;
+                    }
                 }
+                _ => {}
             }
-            _ => {}
-        }
-        let mut diff_x = head.0 - tail.0;
-        let mut diff_y = head.1 - tail.1;
-        if diff_x.abs() < 2 && diff_y.abs() < 2 {
-            continue;
-        }
-        if diff_x.abs() == 1 {
-            tail.0 = head.0;
-            diff_x = 0;
-        } else if diff_y.abs() == 1 {
-            tail.1 = head.1;
-            diff_y = 0;
-        }
-        if diff_x == 0 {
-            if head.1 > tail.1 {
-                while head.1 > tail.1 + 1 {
-                    tail.1 += 1;
-                    add_to_visited(tail.0, tail.1);
+            knots[head_index] = head;
+            let mut diff_x = head.0 - tail.0;
+            let mut diff_y = head.1 - tail.1;
+            if diff_x.abs() < 2 && diff_y.abs() < 2 {
+                continue;
+            }
+            if diff_x.abs() == 1 {
+                tail.0 = head.0;
+                diff_x = 0;
+            } else if diff_y.abs() == 1 {
+                tail.1 = head.1;
+                diff_y = 0;
+            }
+            if diff_x == 0 {
+                if head.1 > tail.1 {
+                    while head.1 > tail.1 + 1 {
+                        tail.1 += 1;
+                        if tail_index == NUMBER_OF_KNOTS as usize - 1 {
+                            add_to_visited(tail.0, tail.1);
+                        }
+                    }
+                } else {
+                    while head.1 < tail.1 - 1 {
+                        tail.1 -= 1;
+                        if tail_index == NUMBER_OF_KNOTS as usize - 1 {
+                            add_to_visited(tail.0, tail.1);
+                        }
+                    }
                 }
             } else {
-                while head.1 < tail.1 - 1 {
-                    tail.1 -= 1;
-                    add_to_visited(tail.0, tail.1);
+                // diff_y = 0
+                if head.0 > tail.0 {
+                    while head.0 > tail.0 + 1 {
+                        tail.0 += 1;
+                        if tail_index == NUMBER_OF_KNOTS as usize - 1 {
+                            add_to_visited(tail.0, tail.1);
+                        }
+                    }
+                } else {
+                    while head.0 < tail.0 - 1 {
+                        tail.0 -= 1;
+                        if tail_index == NUMBER_OF_KNOTS as usize - 1 {
+                            add_to_visited(tail.0, tail.1);
+                        }
+                    }
                 }
             }
-        } else { // diff_y = 0
-            if head.0 > tail.0 {
-                while head.0 > tail.0 + 1 {
-                    tail.0 += 1;
-                    add_to_visited(tail.0, tail.1);
-                }
-            } else {
-                while head.0 < tail.0 - 1 {
-                    tail.0 -= 1;
-                    add_to_visited(tail.0, tail.1);
-                }
-            }
-        } 
+            knots[tail_index] = tail;
+        }
     }
-    // Part 1
+    // Part 1 (Ans for day.txt : 6354)
     println!("The tail visits {} locations", visited.len());
 }
